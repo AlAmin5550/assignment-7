a9eeeb8bfafa14443ba8fe117a56e8de039af4b6
AlAmin5550 <alamin555021@gmail.com>
2024-03-29 00:30:26 +0600

    commiting for first time

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      
+      <main>
+        <Header></Header>
+      </main>
+      
+    </>
+  )
+}
+
+export default App
